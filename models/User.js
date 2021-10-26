9e573243c0407664554276f357444f8c39691d2d
Angela Loftus <angelaloftus@yahoo.com>
2021-10-26 16:22:25 -0500

    add password hashing

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const {Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+const { beforeCreate } = require('../config/connection');
 
 //create our User model
 class User extends Model {}
@@ -46,6 +48,29 @@ User.init(
         }        
     },
     {
+        // //clunky version with promises
+        // // hooks: {
+
+        // //     //set up beforeCreate lifecycle "hook" functionality
+        // //     beforeCreate(userData) {
+        // //         return bcrypt.hash(userData.password, 10).then(newUserData => {
+        // //             return newUserData
+        // //         });
+        // //     }
+        // // },
+        // //async/await syntax, better version
+        hooks: {
+            //set up beforeCreate lifecycle 'hook' functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            //set up beforeUpdate lifecycle 'hook' functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
