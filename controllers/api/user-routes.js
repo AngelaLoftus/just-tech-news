a63fa6cdd06dab35850b39c1577659771d94db56
Angela Loftus <angelaloftus@yahoo.com>
2021-11-02 17:11:11 -0500

    added sessions to login and logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { runInNewContext } = require('vm');
 const { User, Post, Vote, Comment } = require('../../models');
 
 //GET /api/users
@@ -64,11 +65,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
 });
 
 router.post('/login', (req, res)=> {
@@ -90,7 +95,14 @@ router.post('/login', (req, res)=> {
             return;
         }
 
-        res.json({ user: dbUserData, message: "You are now logged in." });
+        req.session.save(()=> {
+            //declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
     });
 });
 
@@ -138,5 +150,18 @@ router.delete('/:id', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn){
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
+
+
 
 module.exports = router; 
