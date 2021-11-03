ce85f0cbad4072a72889ba260970b04aa8298139
Angela Loftus <angelaloftus@yahoo.com>
2021-11-03 13:31:43 -0500

    comment form and logout

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -10,37 +10,23 @@ router.get('/', (req, res) => {
     })
 });
 
-router.post('/', (req,res) => {
-    Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+router.post('/', (req, res) => {
+    //check the session
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            //use the id from the session
+            user_id: req.session.user_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
 });
 
-// router.delete('/:id', (req, res) => {
-//     Post.destroy({
-//         where: {
-//             id: req.params.id
-//         }
-//     })
-//     .then(dbPostData => {
-//         if(!dbPostData) {
-//             res.status(404).json({ message: "No post found with this id" });
-//             return;
-//         }
-//         res.json(dbPostData);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//     });
-// });
 
 router.delete('/:id', (req, res) => {
     Comment.destroy({
